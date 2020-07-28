b3bc08dac9ae3349cd103d52b38aeb0de5f02fcb
Pat Kelly <50188152+patk80@users.noreply.github.com>
2020-07-28 17:12:36 -0400

    Initial Commit
    Adding html/css/js files. Initial commit.

---
+++ scripts.js
@@ -0,0 +1,59 @@
+var flagsArray = [
+    {
+        name: "benin",
+        url: "https://cdn.britannica.com/37/5037-004-E474738C/Flag-Benin.jpg"
+    },
+    {
+        name: "burkina faso",
+        url: "https://upload.wikimedia.org/wikipedia/commons/3/31/Flag_of_Burkina_Faso.svg"
+    },
+    {
+        name: "cambodia",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_Cambodia.svg/1200px-Flag_of_Cambodia.svg.png"
+    },
+    {
+        name: "canada",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/255px-Flag_of_Canada_%28Pantone%29.svg.png"
+    }
+];
+
+// var randomNumber = Math.floor(Math.random() * flagsArray.length);
+
+function randomIndex() {
+    randomNumber = Math.floor(Math.random() * flagsArray.length);
+    return randomNumber;
+}
+
+function randomFlag(){
+    randomIndex();
+    console.log(randomNumber);
+    $("img").attr("src", flagsArray[randomNumber].url);
+}
+
+function checkAnswer() {
+    var userAnswer = $("input").val().toLowerCase();
+    
+    if(userAnswer === flagsArray[randomNumber].name) {
+        console.log("Correct");
+    } else {
+        console.log("WRONG");
+    }
+    $("input").val("");
+}
+
+// generate new random flag //
+$("#random-flag").on("click", function(){
+    randomFlag();
+});
+// check user's answer on submit button click //
+$("#submit-button").on("click", function(){
+    checkAnswer();
+    randomFlag();
+});
+// check user's answer on enter button press //
+$("input").on('keypress',function(e) {
+    if(e.which == 13) {
+        checkAnswer();
+        randomFlag();
+    }
+});
