7c3ce3bc7fbcffb4770956bc5994fc95d0b480e8
patk80 <patk80@gmail.com>
2020-09-11 15:22:50 -0400

    Putting flags in own JS file, linking

---
+++ flags.js
@@ -0,0 +1,218 @@
+const flagsArray = [
+    {
+        name: "algeria",
+        imagePath: "img/Algeria.png"
+    },
+    {
+        name: "angola",
+        imagePath: "img/Angola.png"
+    },
+    {
+        name: "benin",
+        imagePath: "img/Benin.png"
+    },
+    {
+        name: "botswana",
+        imagePath: "img/Botswana.png"
+    },
+    {
+        name: "burkina faso",
+        imagePath: "img/BurkinaFaso.png"
+    },
+    {
+        name: "burundi",
+        imagePath: "img/Burundi.png"
+    },
+    {
+        name: "cameroon",
+        imagePath: "img/Cameroon.png"
+    },
+    {
+        name: "cape verde",
+        imagePath: "img/CapeVerde.png"
+    },
+    {
+        name: "central african republic",
+        imagePath: "img/CentralAfricanRepublic.png"
+    },
+    {
+        name: "chad",
+        imagePath: "img/Chad.png"
+    },
+    {
+        name: "comoros",
+        imagePath: "img/Comoros.png"
+    },
+    {
+        name: "democratic republic of the congo",
+        imagePath: "img/DemocraticRepublicOfTheCongo.png"
+    },
+    {
+        name: "djibouti",
+        imagePath: "img/Djibouti.png"
+    },
+    {
+        name: "egypt",
+        imagePath: "img/Egypt.png"
+    },
+    {
+        name: "equatorial guinea",
+        imagePath: "img/EquatorialGuinea.png"
+    },
+    {
+        name: "eritrea",
+        imagePath: "img/Eritrea.png"
+    },
+    {
+        name: "eswatini",
+        imagePath: "img/Eswatini.png"
+    },
+    {
+        name: "ethiopia",
+        imagePath: "img/Ethiopia.png"
+    },
+    {
+        name: "gabon",
+        imagePath: "img/Gabon.png"
+    },
+    {
+        name: "gambia",
+        imagePath: "img/Gambia.png"
+    },
+    {
+        name: "ghana",
+        imagePath: "img/Ghana.png"
+    },
+    {
+        name: "guinea",
+        imagePath: "img/Guinea.png"
+    },
+    {
+        name: "guinea-bissau",
+        imagePath: "img/Guinea-Bissau.png"
+    },
+    {
+        name: "ivory coast",
+        imagePath: "img/IvoryCoast.png"
+    },
+    {
+        name: "kenya",
+        imagePath: "img/Kenya.png"
+    },
+    {
+        name: "lesotho",
+        imagePath: "img/Lesotho.png"
+    },
+    {
+        name: "liberia",
+        imagePath: "img/Liberia.png"
+    },
+    {
+        name: "libya",
+        imagePath: "img/Libya.png"
+    },
+    {
+        name: "madagascar",
+        imagePath: "img/Madagascar.png"
+    },
+    {
+        name: "malawi",
+        imagePath: "img/Malawi.png"
+    },
+    {
+        name: "mali",
+        imagePath: "img/Mali.png"
+    },
+    {
+        name: "mauritania",
+        imagePath: "img/Mauritania.png"
+    },
+    {
+        name: "mauritius",
+        imagePath: "img/Mauritius.png"
+    },
+    {
+        name: "morocco",
+        imagePath: "img/Morocco.png"
+    },
+    {
+        name: "mozambique",
+        imagePath: "img/Mozambique.png"
+    },
+    {
+        name: "namibia",
+        imagePath: "img/Namibia.png"
+    },
+    {
+        name: "niger",
+        imagePath: "img/Niger.png"
+    },
+    {
+        name: "nigeria",
+        imagePath: "img/Nigeria.png"
+    },
+    {
+        name: "republic of the congo",
+        imagePath: "img/RepublicOfTheCongo.png"
+    },
+    {
+        name: "rwanda",
+        imagePath: "img/Rwanda.png"
+    },
+    {
+        name: "sao tome and principe",
+        imagePath: "img/SaoTomeAndPrincipe.png"
+    },
+    {
+        name: "senegal",
+        imagePath: "img/Senegal.png"
+    },
+    {
+        name: "seychelles",
+        imagePath: "img/Seychelles.png"
+    },
+    {
+        name: "sierra leone",
+        imagePath: "img/SierraLeone.png"
+    },
+    {
+        name: "somalia",
+        imagePath: "img/Somalia.png"
+    },
+    {
+        name: "south africa",
+        imagePath: "img/SouthAfrica.png"
+    },
+    {
+        name: "south sudan",
+        imagePath: "img/SouthSudan.png"
+    },
+    {
+        name: "sudan",
+        imagePath: "img/Sudan.png"
+    },
+    {
+        name: "tanzania",
+        imagePath: "img/Tanzania.png"
+    },
+    {
+        name: "togo",
+        imagePath: "img/Togo.png"
+    },
+    {
+        name: "tunisia",
+        imagePath: "img/Tunisia.png"
+    },
+    {
+        name: "uganda",
+        imagePath: "img/Uganda.png"
+    },
+    {
+        name: "zambia",
+        imagePath: "img/Zambia.png"
+    },
+    {
+        name: "zimbabwe",
+        imagePath: "img/Zimbabwe.png"
+    }
+];
