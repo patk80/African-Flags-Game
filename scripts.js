98d15df902ed9193e675478fb7c41d8fae21bb5d
Pat Kelly <PatK80@gmail.com>
2021-11-15 21:45:48 -0500

    Adding comments to JS file

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
+// function to generate random index number from flags.js file //
 function randomIndex() {
     randomNumber = Math.floor(Math.random() * flagsArray.length);
     return randomNumber;
 }
 
+// function to display the flag of the random index number generated in randomIndex function //
 function randomFlag(){
     randomIndex();
     $("img").attr("src", flagsArray[randomNumber].imagePath);
 }
 
+// generate new random flag when user clicks "Start" button //
+$("#start-button").on("click", function(){
+    randomFlag();
+    $("#start-button").css("display", "none");
+    $("#input-for-answers").css("visibility", "visible");
+    $("#submit-button").css("visibility", "visible");
+});
+
+// function to evaluate the user's input //
 function checkAnswer() {
     var userAnswer = $("input").val().toLowerCase();
     
@@ -22,18 +33,12 @@ function checkAnswer() {
     $("input").val("");
 }
 
-// generate new random flag //
-$("#start-button").on("click", function(){
-    randomFlag();
-    $("#start-button").css("display", "none");
-    $("#input-for-answers").css("visibility", "visible");
-    $("#submit-button").css("visibility", "visible");
-});
 // check user's answer on submit button click //
 $("#submit-button").on("click", function(){
     checkAnswer();
     randomFlag();
 });
+
 // check user's answer on enter button press //
 $("input").on('keypress',function(e) {
     if(e.code === "Enter") {
